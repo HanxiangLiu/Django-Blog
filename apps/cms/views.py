a7c4b03ae18c4359f3ba23143aedc241f796d7af
HanxiangLiu <hanxiangliu529@gmail.com>
2024-01-18 02:58:51 -0800

    Article Publish Page

--- apps/cms/views.py
+++ apps/cms/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from apps.post.models import Category, Post
+from apps.bloguser.models import User
 
 
 def cms_login(request):
@@ -36,4 +37,9 @@ def post_manage_view(request):
 
 
 def post_publish_view(request):
-    return render(request, 'cms/post/publish.html')
+    context = {
+        'list_data_category': Category.objects.all(),
+        'list_data_user': User.objects.all(),
+        'list_data_status': Post.STATUS_ITEMS
+    }
+    return render(request, 'cms/post/publish.html', context=context)
