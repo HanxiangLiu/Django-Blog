892f918d2d9a073fa2505563fbca897034ed1f56
HanxiangLiu <hanxiangliu529@gmail.com>
2024-01-18 23:14:32 -0800

    Article Detail Page

--- apps/post/urls.py
+++ apps/post/urls.py
@@ -6,4 +6,5 @@ app_name = "post"
 urlpatterns = [
     path("", views.index, name="index"),
     path("list/", views.post_list_view, name="post_list"),
+    path("detail/<int:time_id>/", views.detail, name="detail"),
 ]
--- apps/post/views.py
+++ apps/post/views.py
@@ -7,3 +7,7 @@ def index(request):
 
 def post_list_view(request):
     return render(request, 'post/list.html')
+
+
+def detail(request, time_id):
+    return render(request, 'post/detail.html')
