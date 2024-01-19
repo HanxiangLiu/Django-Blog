6925b98a9fb7c66297622546bc8ebc78a40581da
HanxiangLiu <hanxiangliu529@gmail.com>
2024-01-18 16:47:53 -0800

    import Bootstrap

--- Peekpa/urls.py
+++ Peekpa/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('apps.post.urls')),
     path("admin/", admin.site.urls),
     path('cms/', include('apps.cms.urls')),
     path('account/', include('apps.bloguser.urls')),
